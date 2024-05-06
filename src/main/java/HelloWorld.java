5b506b690e45a4a6430757d20c52eb4c2dd05d7b
fabiovincenzi <93596376+fabiovincenzi@users.noreply.github.com>
2024-05-06 11:13:09 +0200

    feat!: add important stuff
    * add CI
    
    * fix ci
    
    * fix ci
    
    * feat: add very good method
    
    * chore: add java doc
    
    ---------
    
    Co-authored-by: fabio vincenzi <fabio.vincenzi2@studio.unibo.it>

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,4 +2,11 @@ public class HelloWorld {
     public static void main(String[] args) {
 	System.out.println("Hello World!");
     }
+
+    /**
+     * This method prints the name of the current class
+     */
+    public static void printClassName() {
+        System.out.println("HelloWorld");
+    }
 }
